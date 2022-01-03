35c136820c6c8797b1d69482324b46d0f32a6deb
pooriaaskarim <pooriaaskarim@gmail.com>
2022-01-03 11:08:59 +0330

    -Search Page Structure implemented

---
+++ lib/src/pages/search/views/search_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:shopping_app/src/pages/search/controllers/search_controller.dart';
+
+class SearchPage extends StatelessWidget {
+  SearchPage({Key? key}) : super(key: key);
+  final controller = Get.find<SearchController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          actions: [TextFormField()],
+        ),
+        body: SingleChildScrollView(
+            // child:
+            ));
+  }
+}
