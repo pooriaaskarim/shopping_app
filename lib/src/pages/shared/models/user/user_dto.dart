06af904ae80969ecfaa9f814b47aea2fa84087c4
pooriaaskarim <pooriaaskarim@gmail.com>
2022-01-03 09:32:39 +0330

    -Changes in SignUp models and DTOS.

--- lib/src/pages/shared/models/user/user_dto.dart
+++ lib/src/pages/shared/models/user/user_dto.dart
@@ -1,11 +1,14 @@
 abstract class UserDTO {
   String name, surname, username, password, address;
+  int imageId;
   bool isAdmin;
+
   UserDTO(
       {required this.name,
       required this.surname,
       required this.username,
       required this.password,
       required this.address,
+      required this.imageId,
       this.isAdmin = false});
 }
